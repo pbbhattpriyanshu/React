e9501bff7b411f64ecd8e07e25f7006e0074ea3d
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-05-26 23:21:22 +0530

    integrating API and useEFFECT

--- 03Tailwindcss/src/App.jsx
+++ 03Tailwindcss/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Lec01 from './Class/Lec01.jsx'
 import Lec02 from './Class/Lec02.jsx'
 import Navbar from './Components/Navbar.jsx'
 import Footer from './Components/Footer.jsx'  
 import Card from './Components/Card.jsx'
 import Ex from './Components/Ex.jsx'
-
+import axios from 'axios'
 
 
 function App() {
@@ -52,7 +52,18 @@ function App() {
 //     "profilePhoto": "https://randomuser.me/api/portraits/men/21.jpg"
 //   }
 // ]
+const [data, setData] = useState([])
 
+const getData = async () => {
+  const response = await axios.get('https://picsum.photos/v2/list')
+  setData(response.data)
+}
+
+useEffect(() => {
+  // This will run once when the component mounts
+  getData()
+}
+, [])
   return (
     <>
     {/*Above data props logic by maping */}
@@ -61,7 +72,17 @@ function App() {
         return <Card key={idx} user={elem.name} city={elem.city} age={elem.age} profession={elem.profession} salary={elem.salary} photo={elem.profilePhoto}/>
       })}
     </div> */}
-    <Ex name='Piyush' />
+    {/* <Ex name='Piyush' /> */}
+
+    <button onClick={getData} className='bg-teal-700 text-white font-semibold text-xl px-6 py-3 rounded m-2 p-10 active:scale-90'>Get Data</button>
+    <div className='bg-grey-800 p-5'>
+      {data.map(function(elem, idx) {
+        return <div key={idx} className='bg-white text-black flex item-center justify-center w-full px-7 py-6 rounded mb-2' >
+          <img className='h-40' src={elem.download_url} alt="" />
+          <h1 className='text-xl font-semibold text-black ml-4'>{elem.author}</h1>
+        </div>
+      })}
+    </div>
     </>
   )
 }
