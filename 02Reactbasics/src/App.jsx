b76b1b720956adb125e8def44e7d895df1a84f78
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-20 10:29:57 +0530

    `Removed React context and components, updated package-lock.json and main.jsx`

--- 02Reactbasics/src/App.jsx
+++ 02Reactbasics/src/App.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import { CounterContext } from "./context/context";
 function App() {
-  //useState
-  const [count, setCount] = useState(0); // Declaring state variable 'count' with initial value 0
 
   return (
     <>
-      {" "}
-      {/* Fragment to group elements without adding extra nodes */}
-      <CounterContext.Provider value={{count, setCount}}>
-        <h1>React Basics</h1>
-        <p>Welcome to React Basics - useContext</p>
-        <h5>
-          Conponents: Passing count as props. <Navbar count={count} />{" "}
-        </h5>
-        <br /> <br />
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 10)}>Increment</button>
-        <br /> <br />
-        <button onClick={() => setCount(count - 10)}>Decrement</button>
-      </CounterContext.Provider>
+    <h1>Welcome</h1>
     </>
   );
 }
