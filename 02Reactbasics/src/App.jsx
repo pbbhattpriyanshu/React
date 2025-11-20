936bd4c5c17a785e2b36098f27b00f1a779cc525
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-11-20 12:55:41 +0530

    `Updated App component to render multiple Card components with dynamic props`

--- 02Reactbasics/src/App.jsx
+++ 02Reactbasics/src/App.jsx
@@ -4,9 +4,10 @@ function App() {
   return (
     <>
       <h1>Welcome</h1>
-      <Card />
-      <Card />
-      <Card />
+      <div className="container">
+        <Card pic='https://images.unsplash.com/photo-1588305665522-1c6af1f69b09?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' name='Priyanshu' caption='This my beatifull uk'/>
+        <Card pic='https://images.unsplash.com/photo-1601821139990-9fc929db79ce?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' name='Jenifer' caption='Hello i am jenny'/>
+      </div>
     </>
   );
 }
--- 02Reactbasics/src/Components/Card.jsx
+++ 02Reactbasics/src/Components/Card.jsx
@@ -1,20 +1,17 @@
 import React from "react";
-import './Card.css'
+import "./Card.css";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <>
-      <div className="id-card-outer">
-        <div className="id-card" role="region" aria-label="Student ID Card">
-          <div className="photo-wrap">
-            <img src= 'https://img.freepik.com/free-photo/portrait-young-student-happy-be-back-university_23-2148586577.jpg?semt=ais_hybrid&w=740&q=80' alt={`photo`} />
-          </div>
-
-          <div className="id-footer">Student ID: 2454334</div>
-          <div className="id-name">Name: Jenifer</div>
-          <div className="id-course">Course: MBBS</div>
-          <div className="id-session">Session: 2025-26</div>
-
+      <div className="parent">
+        <div className="card">
+            <img src={props.pic} alt="image" />
+          <h1 className="profileName">{props.name}</h1>
+          <p>
+            {props.caption}
+          </p>
+          <button>View Profile</button>
         </div>
       </div>
     </>
